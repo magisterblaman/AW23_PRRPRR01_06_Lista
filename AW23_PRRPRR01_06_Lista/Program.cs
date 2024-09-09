280fca20ef46b5a443b1c9c374fbcd642b88eac8
magisterblaman <hannes.blaman@lbs.se>
2024-09-09 14:26:32 +0200

    Listor

--- AW23_PRRPRR01_06_Lista/Program.cs
+++ AW23_PRRPRR01_06_Lista/Program.cs
@@ -7,6 +7,39 @@ using System.Threading.Tasks;
 namespace AW23_PRRPRR01_06_Lista {
 	internal class Program {
 		static void Main(string[] args) {
+
+			//string[] petNames = new string[] { 
+			//	"Miss Saigon",
+			//	"Linus",
+			//	"Findus",
+			//	"Pettsson"
+			//};
+
+			List<string> petNames = new List<string>() {
+				"Miss Saigon",
+				"Linus",
+				"Findus",
+				"Pettsson"
+			};
+
+			for (int i = 0; i < petNames.Count; i++) {
+				Console.WriteLine(petNames[i]);
+			}
+			Console.WriteLine("=======");
+
+			petNames.Add("Ola");
+
+			for (int i = 0; i < petNames.Count; i++) {
+				Console.WriteLine(petNames[i]);
+			}
+			Console.WriteLine("=======");
+
+			petNames.RemoveAt(2);
+
+			for (int i = 0; i < petNames.Count; i++) {
+				Console.WriteLine(petNames[i]);
+			}
+
 		}
 	}
 }
